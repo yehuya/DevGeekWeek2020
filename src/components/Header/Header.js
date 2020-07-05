e1af64883f1466f8672e3711aa403dde976fb09e
yehuda <yehuda@libi.team>
2020-07-05 23:18:48 +0300

    Fix users length issue

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,12 @@ import React, { useContext, useRef, useEffect } from 'react';
 import UsersContext from '../../context/UsersContext';
 
 const Header = () => {
-    const {users} = useContext(UsersContext);
+    const users = useContext(UsersContext);
 
     return (
         <header>
             <h1>
-                Users <small>{users && users.length}</small>
+                Users <small>{users.length}</small>
             </h1>
         </header>
     )
